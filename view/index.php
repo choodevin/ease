8a0a30d1b48645a72b29c5fb374a5a019ab0d70c
devin <devinchoo99@outlook.com>
2021-10-16 13:12:40 +0800

    Project Creation

---
+++ view/index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>PHP Test</title>
+    <link href=""
+</head>
+
+<body>
+    <p>Hello World</p>
+</body>
+
+<script>
+    var bootstrap_enabled = (typeof $().modal == 'function');
+    console.log(bootstrap_enabled);
+</script>
+
+</html>
